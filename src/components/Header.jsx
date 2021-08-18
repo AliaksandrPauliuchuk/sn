74139af180ec1a4b7dce4ba66b04c5709d74b8b1
Aliaksandr Pauliuchuk <aliaksandr.pauliuchuk@gmail.com>
2021-08-18 11:14:00 +0300

    l14

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import s from "./Header.module.css";
+
+const Header = () => {
+  return (
+    <header className={s.header}>
+      <img src="https://images-platform.99static.com//pzcJOHF-ZiSByHTQVTMAtUQkhBw=/164x165:792x793/fit-in/500x500/99designs-contests-attachments/111/111309/attachment_111309139" />
+    </header>
+  );
+};
+export default Header;
